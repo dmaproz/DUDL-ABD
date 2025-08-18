cdc16be72ece94ee1b55d17d5ffb182eda5ac456
AnaIsabeldaFonseca <fonseca.anaisabel2@gmail.com>
2025-08-18 18:27:10 -0300

    Create my_code.py

---
+++ project2/my_code.py
@@ -0,0 +1,84 @@
+# Activate Environment: .\venv\Scripts\activate
+
+
+# Project 2: Predicting heart disease
+
+# Data information:
+# https://archive.ics.uci.edu/ml/datasets/Heart+Disease
+
+# Data source
+# https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data
+
+### import libraries
+
+# for DL modeling
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader,TensorDataset
+from sklearn.model_selection import train_test_split
+
+# for number-crunching
+import numpy as np
+
+# for dataset management
+import pandas as pd
+
+# for data visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+# from IPython.display import set_matplotlib_formats
+# set_matplotlib_formats('svg')
+
+import matplotlib_inline.backend_inline
+if isinstance('svg', str):
+
+  ipython_format = ['svg']
+
+matplotlib_inline.backend_inline.set_matplotlib_formats(*ipython_format)
+
+
+# import the data
+url  = 'https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data'
+data = pd.read_csv(url,sep=',',header=None)
+data.columns = ['age','sex','cp','trestbps','chol','fbs','restecg','thalach','exang','oldpeak','slope','ca','thal','DISEASE']
+
+
+# data contain some ?'s; replace with NaN and drop those rows
+data = data.replace('?',np.nan).dropna()
+
+data
+
+# describe the data
+data.describe()
+
+# check the distributions
+fig,ax = plt.subplots(1,figsize=(17,4))
+ax = sns.boxplot(data=data)
+ax.set_xticklabels(ax.get_xticklabels(),rotation=45)
+plt.show()
+
+# z-score the non-categorical columns
+cols2zscore = data.keys()
+cols2zscore = cols2zscore.drop(['sex','fbs','exang','DISEASE'])
+cols2zscore
+
+for c in cols2zscore:
+  d = pd.to_numeric(data[c]) # force to numeric (addresses some data-format issues)
+  data[c] = (d - d.mean())/d.std(ddof=1)
+  
+# check the distributions again
+fig,ax = plt.subplots(1,figsize=(17,4))
+ax = sns.boxplot(data=data)
+ax.set_xticklabels(ax.get_xticklabels(),rotation=45)
+plt.show()
+
+# count of all unique types of 'DISEASE'
+data['DISEASE'].value_counts()
+
+# re-code events to 0 (absent) and 1 (present)
+data['DISEASE'][data['DISEASE']>0] = 1
+
+# Re-organize the data: train/test in DataLoaders
+
+#usar cross_emtropy loss intead of MSELoss
